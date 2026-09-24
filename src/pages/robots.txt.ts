f2ae791b3be94df5e49dac74d8f074163cae931b
Cyanvir297Skye <2201599491@qq.com>
2026-09-24 20:52:11 +0800

    fix: robots.txt 显式声明 Content-Type

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -11,5 +11,8 @@ Sitemap: ${sitemapURL.href}
 export const GET: APIRoute = ({ url }) => {
   // url.origin 不含 base 前缀，同 sitemap.xml.ts；astroBase 恒以 '/' 开头
   const sitemapURL = new URL(astroBase + 'sitemap.xml', url.origin);
-  return new Response(getRobotsTxt(sitemapURL));
+  // 裸 new Response(text) 会让响应体没有 Content-Type，靠客户端猜；RFC 9309 规定 robots.txt 是 text/plain。
+  return new Response(getRobotsTxt(sitemapURL), {
+    headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+  });
 };
